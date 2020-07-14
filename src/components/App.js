2344a381fa41248409564526ffa703e588e77658
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-07-14 19:24:04 -0400

    added routing components, switch and header file

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './Header';
+
 function App(props) {
-  return 'Hello world!';
+  return (
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path="/">Homepage</Route>
+        <Route path="/about">About</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Header = (props) => {
+  return (
+    <p>Hello World!</p>
+  );
+}
+
+export default Header;
