0c2d0f5e04d9e8fc5089afe2261bad136b4f0534
Andres Rueda <garo99@gmail.com>
2020-07-14 21:42:46 -0400

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 
 function App(props) {
-  return 'Hello world!';
+  return (
+    <Router>
+      <Switch>
+        <Route path='/'>Homepage</Route>
+        <Route path='/about'>About</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
