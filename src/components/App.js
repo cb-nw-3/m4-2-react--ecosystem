ccb8ec8494bbbf299ad23788ec4c740f9d224448
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-07-14 20:09:13 -0400

    created/added homepage and about components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './Header';
+import Homepage from './Homepage';
+import About from './About';
 
 function App(props) {
   return (
     <Router>
       <Header></Header>
       <Switch>
-        <Route exact={true} path="/">Homepage</Route>
-        <Route path="/about">About</Route>
+        <Route exact={true} path="/">
+          <Homepage />
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
       </Switch>
     </Router>
   );
---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import styled from 'styled-components';
+
+const Homepage = () => {
+  return (
+    <p>Fruit emporium sells the finest fruits from this world and beyond.</p>
+  );
+
+}
+
+export default Homepage;
