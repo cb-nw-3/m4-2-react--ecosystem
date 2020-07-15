52111171c25e0947e3b3a86447be9f44f474eb75
donnyesq <adonis@darmas.ca>
2020-07-14 23:15:52 -0400

    grid display applied to homepage

--- src/components/ListingGrid.js
+++ src/components/ListingGrid.js
@@ -1,18 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(3, 1fr);
+`;
 
 const ListingGrid = ({ itemList }) => {
   const items = Object.values(itemList);
 
   return (
-    <div>
+    <Wrapper>
       {items.map((item, index) => (
         <div key={item.name + index}>
-          <img src={item.imageSrc} alt={item.name}></img>
+          <img
+            src={item.imageSrc}
+            alt={item.name}
+            style={{ width: "200px" }}
+          ></img>
           <h3>{item.name}</h3>
           <p>{item.latinName}</p>
         </div>
       ))}
-    </div>
+    </Wrapper>
   );
 };
 
