812b18330a063a6142b6ce0fc98d0eae141b5713
Roger Lam <rogerlam@wylco.org>
2020-07-15 20:59:32 -0400

    some progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import GlobalStyle from './GlobalStyles'
+
+function Header() {
+  return (
+    <Wrapper>
+      <Title>Fruit Emporium</Title>
+      <Navbar>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </Navbar>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  /* Other styles here */
+`;
+
+const Navbar = styled.div`
+  border: 2px solid orange;
+  padding-left: 20px;
+`;
 
 function App(props) {
-  return 'Hello world!';
+  return (
+    <div>
+      <GlobalStyle />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/">Homepage</Route>
+          <Route path="/about">About</Route>
+          <Route path="/items/:itemId">Items</Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Raleway', sans-serif;
+}
+
+h1 {
+    font-size: 40px;
+    font-weight: bold;
+}
+`
+
+export default GlobalStyle;
