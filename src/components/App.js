d0ba6028c2d7c2562d493286f0c02039446780ef
jenny ma <jenny_lau_8@hotmail.com>
2020-07-16 21:36:18 -0400

    About.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Wrapper } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Homepage from "./Homepage";
 import About from "./About";
 import ItemDetails from "./ItemsDetails";
 import Header from './Header';
 import GlobalStyles from './GlobalStyles';
 
+
 function App(props) {
   return (
-  <BrowserRouter>
-    <Wrapper>
+  <Router>
+    
       <Header></Header>
       <Switch>
       <Route path="/"><Homepage></Homepage></Route>
       <Route path="/about"><About></About></Route>
       <Route path="/items/:itemId"><ItemDetails></ItemDetails></Route>
       </Switch>
-    </Wrapper>
+    
     <GlobalStyles></GlobalStyles>
-  </BrowserRouter>
+  </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Wrapper, Title, NavigationLink } from 'react-dom';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
     return (
         <Wrapper>
             <Title>Fruit Emporium</Title>
-            <NavigationLink to="/">Home</NavigationLink>
-            <NavigationLink to="/about">About</NavigationLink>
+            <StyledLink to="/">Home</StyledLink>
+            <StyledLink to="/about">About</StyledLink>
         </Wrapper>
     );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  /* Other styles here */
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  /* Other styles here */
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #6b45e7;
+`;
+
 export default Header;
