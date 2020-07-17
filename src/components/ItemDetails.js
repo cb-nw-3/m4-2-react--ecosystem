f2680e84ed477f943ef4dfc58f2d6d501d6d067a
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-07-16 22:36:50 -0400

    rendered item details page

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,15 +1,101 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-//import styled from 'styled-components';
+import styled from 'styled-components';
+
+import { items } from '../data';
+
+const CardWrapper = styled.div`
+  display: flex;
+  padding: 0 5%;
+  margin-top: 3%;
+  /* justify-content: center; */
+`;
+
+const ItemImg = styled.img`
+  width: 350px;
+  height: 400px;
+  border-radius: 20px;
+`;
+
+const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 4%;
+`;
+
+const ItemName = styled.h1`
+  font-size: 2.7rem;
+  font-weight: bolder;
+`;
+
+const ItemLatinName = styled.h2`
+  color: grey;
+  opacity: 0.8;
+  font-style: italic;
+  font-size: 22px;
+  margin-bottom: 20px;
+`;
+
+const Text = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 20px 0;
+`;
+
+const TextOrigin = styled.p`
+  font-size: 18px;
+  margin: 20px 0 40px;
+  font-style: italic;
+`;
+
+const Button = styled.button`
+  width: 280px;
+  height: 60px;
+  border-radius: 10px;
+  font-size: 22px;
+  color: white;
+  background: #4C0CD4;
+  border: none;
+  outline: none;
+  font-family: 'Raleway', sans-serif;
+
+  &:hover {
+    background: #FF0073;
+    font-weight: bold;
+    outline: none;
+    cursor: pointer;
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
 
 
 
 const ItemDetails = () => {
+  
+  //returns the :itemId from the url parameters
   const { itemId } = useParams();
+
+  //from the { items } data, access the item's object via it's ID
+  const item = items[itemId];
+  
+  //console.log(item);
+
   return (
-    <>
-      <p>{itemId}</p>
-    </>
+    <CardWrapper>
+      <ItemImg src={item.imageSrc}></ItemImg>
+      <CardContent>
+        <ItemName>{item.name}</ItemName>
+        <ItemLatinName>{item.latinName}</ItemLatinName>
+        <Text>{item.description}</Text>
+        <TextOrigin>
+          Product of <strong>{item.countryOfOrigin}</strong>
+        </TextOrigin>
+        <Button>{`$${item.price} - Buy now`}</Button>
+      </CardContent>
+    </CardWrapper>
   );
 }
 
