b6594f9a8f341a87b85a03a70295113701b2ea92
Aquarius-beans <rare.beans1959@gmail.com>
2020-07-18 17:18:05 -0400

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled, { css } from "styled-components";
 
-function App(props) {
-  return 'Hello world!';
+import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+
+import GlobalStyles from "./GlobalStyles";
+import ItemDetails from "./ItemDetails";
+
+function App() {
+    return (
+        <Router>
+            <Wrapper>
+                <Header />
+                <Main>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/items/:itemId">
+                            <ItemDetails />
+                        </Route>
+                    </Switch>
+                </Main>
+            </Wrapper>
+
+            <GlobalStyles />
+        </Router>
+    );
 }
+
+const Wrapper = styled.div`
+    max-width: 800px;
+    margin: auto;
+`;
+
+const Main = styled.main`
+    padding-top: 32px;
+    padding-bottom: 32px;
+`;
 
 export default App;
