765f6abe4efdd4969540f58dc688d7610ee823c4
Alex Langevin <alex_langevin@hotmail.ca>
2020-07-24 23:09:09 -0400

    Exercice 1 completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,31 +7,49 @@ function Header() {
         <Wrapper>
             <Logo>Fruit Emporium</Logo>
             <nav>
-                <ul>
+                <NavList>
                     <li>
                         <NavigLink to='/'>Homepage</NavigLink>
                     </li>
                     <li>
                         <NavigLink to='/about'>About</NavigLink>
                     </li>
-                </ul>
+                </NavList>
             </nav>
         </Wrapper>
     ) 
 }
 
 const Wrapper = styled.header`
+    width:100%;
     display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 const Logo = styled.h1`
+    margin-left:50px;
     font-size: 40px;
     font-weight: bolder;
 `;
 
+const NavList = styled.ul`
+    list-style-type: none;
+    min-width: 300px;
+    display:flex;
+    justify-content: space-space-evenly;
+`
+
 const NavigLink = styled(NavLink)`
-    color: blue;
+    font-size: 20px;
+    text-decoration: none;
+    padding: 0 25px;
     font-weight: bold;
+
+    &:hover {
+        text-decoration: underline;
+        color: darksalmon;
+    }
 `;
 
 export default Header;
