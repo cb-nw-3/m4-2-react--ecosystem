485c5572ece22df725fae7b5fd1c00cbe8133f3a
Hiba Z <hiba4.z@gmail.com>
2020-07-25 23:53:24 -0400

    WORKSHOP M4-2 COMPLETED

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./Header";
+import About from "./About";
+import Home from "./Home";
+import ItemDetails from "./ItemDetails";
 
 function App(props) {
-  return 'Hello world!';
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+          About
+        </Route>
+        <Route exact path="/items/:itemId">
+          <ItemDetails />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
-
+// Router({exact: true, path: "/"}, <p>)
 export default App;
